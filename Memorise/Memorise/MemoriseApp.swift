0612bdb6774a219d6c0cde39a320c4e790e3ef9e
Ankit Goyal <ankitgoyal21302+github@gmail.com>
2024-09-08 16:30:14 +0530

    Starter Project

---
+++ Memorise/Memorise/MemoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseApp.swift
+//  Memorise
+//
+//  Created by Ankit Goyal J on 08/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
